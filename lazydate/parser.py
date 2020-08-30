e39dbca2ed63299c14f47bc43b3f9db6c46596be
daniel <daniel@evolution.ai>
2020-08-30 15:45:39 +0100

    stop tensorflow from seeing GPUs on model loading

--- lazydate/parser.py
+++ lazydate/parser.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from typing import Optional
 
+import tensorflow as tf
+
 from lazydate.models import DateModel
+
+tf.config.set_visible_devices([], "GPU")
 
 _date_model = DateModel()
 _date_model.load_weights("saved_models/lstm_date_model_v02")
