cc5cb0b74c57d3ffeedffff1ab2ddf7343df64cd
daniel <daniel@evolution.ai>
2020-09-11 16:03:48 +0100

    fix initial loading of date model

--- lazydate/parser.py
+++ lazydate/parser.py
@@ -7,12 +7,16 @@ import tensorflow as tf
 from lazydate.models import DateModel
 from lazydate.models.config import MAX_SEQUENCE_LEN
 
-# tf.config.set_visible_devices([], "GPU")
-
 logger = logging.getLogger(__name__)
+_date_model = None
 
-_date_model = DateModel()
-_date_model.load_weights("saved_models/lstm_date_model_v02")
+
+def _load_date_model():
+    global _date_model
+    if not _date_model:
+        _date_model = DateModel()
+        _date_model.load_weights("saved_models/lstm_date_model_v02")
+    return _date_model
 
 
 def parse(text: str) -> Optional[datetime]:
@@ -22,7 +26,10 @@ def parse(text: str) -> Optional[datetime]:
             f"{len(text)} > {MAX_SEQUENCE_LEN} - input will be truncated"
         )
 
-    datestr = _date_model.predict(text)
+    with tf.device(f"/CPU:0"):
+        date_model = _load_date_model()
+        datestr = date_model.predict(text)
+
     if datestr == "":
         return None
     return datetime.strptime(datestr, "%Y%m%d")
@@ -39,6 +46,9 @@ def parse_batch(texts: List[str]) -> List[Optional[datetime]]:
             f"{max_length} > {MAX_SEQUENCE_LEN} - input will be truncated"
         )
 
-    datestrs = _date_model.predict_on_batch(texts)
+    with tf.device(f"/CPU:0"):
+        date_model = _load_date_model()
+        datestrs = date_model.predict_on_batch(texts)
+
     dates = [datetime.strptime(d, "%Y%m%d") if d != "" else None for d in datestrs]
     return dates
